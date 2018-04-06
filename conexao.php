3b43b09c98eecc391726a4d8fe25045b171b2048
eduardo <eddfts@gmail.com>
2018-04-06 14:34:18 -0300

    Variaveis de conexão

--- conexao.php
+++ conexao.php
@@ -1,3 +1,13 @@
 <?php
+//abre conexão
+$conn = mysql_connect('localhost', 'root','carcass');
+
+//seleciona banco de dados ativos
+mysql_select_db('estoque', $conn);
+
+//define a consulta que será realizada
+$query = 'Select From produto';
+
+
 
 ?>
