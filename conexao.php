781d3ce6704349312e3dfa7c52c7534ead6b7206
eduardo <eddfts@gmail.com>
2018-04-10 10:58:23 -0300

    conexao php com mysqli

--- conexao.php
+++ conexao.php
@@ -1,46 +1,27 @@
 <?php
-//abre conexão
-/*$conn = mysql_connect('localhost', 'root','carcass');
-
-//seleciona banco de dados ativos
-mysql_select_db('estoque', $conn);
-
-//define a consulta que será realizada
-$query = 'select id,nome  from produto';
-
-//envia consulta ao banco de dados
-$result = mysql_query($query, $conn);
-
-if($result){
-    while ($row = mysql_fetch_assoc($result)){
-        print  $row['id'].'-'.$row['nome']."<br>";
-    }
-}*/
-
-//mysql_close($conn);
-
-$username = 'root';
-$password = 'carcass';
-
-$parametro_id = 1;
-try {
-  $conn = new PDO('mysql:host=localhost;dbname=estoque', $username, $password);
-  $stmt = $conn->prepare('SELECT * FROM produto WHERE id = :$parametro_id);
-  $stmt->execute(array('id' => $id));
+$link = mysqli_connect("localhost", "root", "carcass", "estoque");
  
-  $result = $stmt->fetchAll();
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging erro: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
  
-  if ( count($result) ) { 
-    foreach($result as $row) {
-      print_r($row);
-    }   
-  } else {
-    echo "Nenhum resultado retornado.";
-  }
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
+echo "Sucesso: Sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+/*$db = mysqli_select_db($cx, "estoque");*/
+
+$sql = mysqli_query($link, "SELECT * FROM produto") or die(
+	mysqli_error($link) //caso haja um erro na consulta
+);
+
+while($aux = mysqli_fetch_assoc($sql)) { echo "-----------------------------------------
+  "; echo "id:".$aux["id"]."
+  "; echo "nome:".$aux["nome"]."
+  ";
 }
 
- 
+
+mysqli_close($link);
 
 ?>
